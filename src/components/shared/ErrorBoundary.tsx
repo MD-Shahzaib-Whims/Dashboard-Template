0bc4757abd115d5e5634396a0386cce3ee99a1e9
MD-Shahzaib <dev7.whimstech@gmail.com>
2025-02-27 22:51:43 +0500

    Enhance ErrorBoundary component to log errors and display error messages for better debugging

--- src/components/shared/ErrorBoundary.tsx
+++ src/components/shared/ErrorBoundary.tsx
@@ -8,14 +8,15 @@ export default function ErrorBoundary({
     error: Error
     reset: () => void
 }>) {
-    
+
     useEffect(() => {
-        console.error(error)
+        console.log("error ---------------------> :", error)
     }, [error])
 
     return (
         <div className="flex flex-col items-center justify-center h-full">
             <h2 className="text-2xl font-bold mb-4">Something went wrong!</h2>
+            <p className='text-sm font-medium pb-4'>{JSON.stringify(error.message)}</p>
             <button
                 className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
                 onClick={() => reset()}
